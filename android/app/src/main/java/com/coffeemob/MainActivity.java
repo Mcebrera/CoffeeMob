76644d93db161a25395d316b3adeeff627bc444d
Michael Cebrera <macebrera@wgo.net>
2023-04-23 21:26:03 -0500

    screens and more set up

--- android/app/src/main/java/com/coffeemob/MainActivity.java
+++ android/app/src/main/java/com/coffeemob/MainActivity.java
@@ -1,3 +1,4 @@
+import android.os.Bundle;
 package com.coffeemob;
 
 import com.facebook.react.ReactActivity;
@@ -14,6 +15,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "CoffeeMob";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
---
+++ src/screens/InstructionsScreen.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const InstructionsScreen = () => {
+    return <View />;
+};
+
+export default InstructionsScreen;
